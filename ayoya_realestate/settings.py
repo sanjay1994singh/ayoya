8c94d7a0a0b434e95aeafb77b76f3414972cf769
sanjay1994singh <sanjaypython22@gmail.com>
2026-06-21 23:02:56 +0530

    p

--- ayoya_realestate/settings.py
+++ ayoya_realestate/settings.py
@@ -64,10 +64,21 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "ayoya_realestate.wsgi.application"
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv("DB_NAME", ""),
+        'USER': os.getenv("DB_USER", ""),
+        'PASSWORD': os.getenv("DB_PASSWORD", ""),
+        'HOST': os.getenv("DB_HOST", "127.0.0.1"),
+        'PORT': os.getenv("DB_PORT", "3306"),
     }
 }
 
